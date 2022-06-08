4095d37c84a1309fbfcb4dc654d539c5c0d5e05c
Yan-Magnan <yanmagnan144@gmail.com>
2022-06-08 19:13:35 -0300

    add a home Page HTML in code - Yan Magnan

--- project/urls.py
+++ project/urls.py
@@ -14,8 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from amigos_panchos.views import home, cardapio, contato, endereco
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('amigos_panchos.urls')),
+    # path('amigos_panchos/', include('amigos_panchos.urls')),
+
+    # path('', home),
+
 ]
