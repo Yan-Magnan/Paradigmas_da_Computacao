113004bc4ea4f34157a41f6804b96ac7f3fcd3e1
Yan-Magnan <yanmagnan144@gmail.com>
2022-06-10 22:26:44 -0300

    NavBar fixed*

--- README.md
+++ README.md
@@ -1,12 +1,8 @@
-# Trabalho de PARADIGMAS DE LINGUAGEM DE PROGRAMAÇÃO
+# Trabalho de PARADIGMAS DE LINGUAGEM DE PROGRAMAÇÃO 
+<<<<<<< HEAD
 *python + Django
 =======
 * Django
 * Python
 * DB_DJANGO
-=======
-
-
-#### Anotações sobre a implementação do back-end:
-* video-aula atual :  43
-* staticfiles-global não implementado - video_aula: 40
+* 
--- amigos_panchos/urls.py
+++ amigos_panchos/urls.py
@@ -3,10 +3,10 @@ from django.urls import path
 from amigos_panchos import views
 
 urlpatterns = [
-    path('', views.home),
-    path('cardapio/', views.cardapio),
-    path('contato/', views.contato),
-    path('endereco/', views.endereco),
+    path('', views.home, name='sobre-nos'),
+    path('cardapio/', views.cardapio, name='cardapio'),
+    path('contato/', views.contato, name='contato'),
+    path('endereco/', views.endereco, name='endereco'),
     # path('', home),
 
 ]
